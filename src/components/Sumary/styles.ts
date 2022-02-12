f7736b84cbfb697d3c255f9f61e534979937d17d
neilson <joseneilson25@gmail.com>
2022-02-12 06:13:23 -0300

    .

--- src/components/Sumary/styles.ts
+++ src/components/Sumary/styles.ts
@@ -5,15 +5,13 @@ export const Container = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 2rem;
-    margin-top: -22rem;
+    margin-top: -25rem;
 
     div{
         background: var(--shape);
         padding: 1.5rem 2rem;
         border-radius: 0.25rem;
         color: var(--text-title);
-       
-    
 
     &.dads{
         color: #FFF;
@@ -30,7 +28,7 @@ export const Container = styled.div`
     strong{
         display: block;
         margin-top: 1rem;
-        font-size 2rem;
+        font-size: 2rem;
         font-weight: 500;
         line-height: 3rem;
     }
