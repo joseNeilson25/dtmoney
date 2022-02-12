971ee382c094e70140a4addd9ca2b6ed5d5963cb
neilson <joseneilson25@gmail.com>
2022-02-12 05:59:34 -0300

    correção do bug da cor do modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -57,6 +57,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     .react-modal-overlay{
+        background: rgba(0,0,0, 0.5);
         position: fixed;
         top: 0;
         bottom: 0;
@@ -72,7 +73,7 @@ export const GlobalStyle = createGlobalStyle`
 
         width: 100%;
         max-width: 576px;
-        background: var (--background);
+        background: var(--background);
         padding: 3rem;
         position: relative;
         border-radius: 0.24rem;
